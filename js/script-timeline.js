d866930cd48e9bf45d31cabd4b5b43c624311580
yanathedev <kochubynska.design@gmail.com>
2023-07-30 23:07:55 +0200

    changed js file

--- js/script-timeline.js
+++ js/script-timeline.js
@@ -1,26 +1,64 @@
-window.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".timeline").forEach(function (e) {
-    let btn = e.querySelectorAll(".btn"),
-      i = e.querySelectorAll(".slider-item");
+// window.addEventListener("DOMContentLoaded", function () {
 
-    btn.forEach(function (click) {
-      click.addEventListener("click", function () {
-        var active = click,
-          datatab = active.getAttribute("data-tab"),
-          selectData = e.querySelector(datatab);
+  // const timeline = document.querySelector(".timeline");
+  // const btns = timeline.querySelectorAll(".btn");
+  // const slider = timeline.querySelectorAll(".slider-item");
 
-        active.classList.contains("btn--active") ||
-          (btn.forEach(function (e) {
-            e.classList.remove("btn--active");
-          }),
-          i.forEach(function (e) {
-            e.classList.remove("item--active");
-          }),
-          active.classList.add("btn--active"),
-          selectData.classList.add("item--active")),
-          console.log(selectData);
-      });
-    }),
-      e.querySelector(".btn").click();
-  });
+  // btns.forEach(function (btn) {
+  //   btn.addEventListener("click", function(){
+  //     const active = btn;
+  //     const datatab = active.getAttribute("data-tab");
+  //     const selectData = timeline.querySelector(datatab);
+
+  //     btns.forEach(function (btn){
+  //       btn.classList.remove("btn--active");
+  //     });
+
+  //     slider.forEach(function(item){
+  //       item.classList.remove("item--active");
+  //     });
+
+  //     active.classList.add("btn--active");
+  //     selectData.classList.add("item--active");
+
+  //     console.log(selectData);
+  //   });
+  // });
+  // btns[0].click();
+
 });
+
+
+const timeline = document.querySelector(".timeline");
+const btns = timeline.querySelectorAll(".btn");
+const slider = timeline.querySelectorAll(".slider-item");
+
+btns.forEach(function(btn) {
+  btn.addEventListener("click", function() {
+    const active = btn;
+    const datatab = active.getAttribute("data-tab");
+    const selectData = timeline.querySelector(datatab);
+
+    btns.forEach(function(btn) {
+      btn.classList.remove("btn--active");
+    });
+
+    slider.forEach(function(item) {
+      item.classList.remove("item--active");
+    });
+
+    active.classList.add("btn--active");
+    selectData.classList.add("item--active");
+
+    console.log(selectData);
+  });
+
+});
+btns[0].click()
+
+
+
+
+
+
+  
