970306829eb457fb006aae1a8f54ba6f1277613c
yanathedev <kochubynska.design@gmail.com>
2023-07-30 23:58:13 +0200

    image directory is changed

--- js/script-timeline.js
+++ js/script-timeline.js
@@ -1,32 +1,32 @@
-// window.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("DOMContentLoaded", function () {
 
-//   const timeline = document.querySelector(".timeline");
-//   const btns = timeline.querySelectorAll(".btn");
-//   const slider = timeline.querySelectorAll(".slider-item");
+  const timeline = document.querySelector(".timeline");
+  const btns = timeline.querySelectorAll(".btn");
+  const slider = timeline.querySelectorAll(".slider-item");
 
-//   btns.forEach(function (btn) {
-//     btn.addEventListener("click", function(){
-//       const active = btn;
-//       const datatab = active.getAttribute("data-tab");
-//       const selectData = timeline.querySelector(datatab);
+  btns.forEach(function (btn) {
+    btn.addEventListener("click", function(){
+      const active = btn;
+      const datatab = active.getAttribute("data-tab");
+      const selectData = timeline.querySelector(datatab);
 
-//       btns.forEach(function (btn){
-//         btn.classList.remove("btn--active");
-//       });
+      btns.forEach(function (btn){
+        btn.classList.remove("btn--active");
+      });
 
-//       slider.forEach(function(item){
-//         item.classList.remove("item--active");
-//       });
+      slider.forEach(function(item){
+        item.classList.remove("item--active");
+      });
 
-//       active.classList.add("btn--active");
-//       selectData.classList.add("item--active");
+      active.classList.add("btn--active");
+      selectData.classList.add("item--active");
 
-//       console.log(selectData);
-//     });
-//   });
-//   btns[0].click();
+      console.log(selectData);
+    });
+  });
+  btns[0].click();
 
-// });
+});
 
 
 
